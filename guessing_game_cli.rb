14020484b20ea7feed0422b0ab8eef2646db5ae6
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-25 10:06:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,12 @@
 require "pry"
 def run_guessing_game
-  guess = "Guess a number betwen 1 and 6."
 	roll = 1+rand(6)
+  guess = "Guess a number betwen 1 and 6."
 # 	wrong = "Sorry! The computerguessed #{roll}."
 	puts guess
 	
-	input_string = gets.chomp
-    if input_string == "exit"
+	input = gets.chomp
+    if input == "exit"
     
       puts "Goodbye!"
       
